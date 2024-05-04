e5a293935661d9ace7e5255a151cb999e8507543
Jhean <drakekhendrick@gmail.com>
2024-05-04 10:53:42 +0800

    apply service to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,30 +1,34 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
-
+import { UsersService } from './users.service';
+import { role } from 'src/model/role';
+import { user } from 'src/model/user';
 @Controller('users')
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService){}
+
     @Get()
-    findAll(@Query('role') role?: 'Intern' | 'Engineer' | 'Admin'){
-        return [role];
+    findAll(@Query('role') role?: role){
+        return this.usersService.findAll(role);
     }
 
     @Get(':id')
     findOne(@Param('id') id : string) {
-        return { id }
+        return this.usersService.findOne(+id);
     }
 
     @Post()
-    create(@Body() user: {}){
-        return user
+    create(@Body() user: user){
+        return this.usersService.create(user);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {}) {
-        return { id, ...userUpdate }
+    update(@Param('id') id: string, @Body() userUpdate: user) {
+        return this.usersService.update(+id, userUpdate)
     }
     
     @Delete(':id')
     delete(@Param('id') id: string){
-        return { id } 
+        return this.usersService.delete(+id);
     }
 } 
